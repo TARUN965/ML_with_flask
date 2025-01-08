61c15beaf31fc95525cce5400721b103df31c2dd
TARUN965 <2311CS020222@MALLAREDDYUNIVERSITY.AC.IN>
2025-01-08 04:51:38 +0000

    tarun

---
+++ TARUNDICT.py
@@ -0,0 +1,12 @@
+import pandas as pd
+screen_time =[1,2,3,4,5]
+sleep_hours = [6,7,8,9,10]
+stu_name =["Vivek","Priyatham","Akhil"]
+dict1 = {
+    "screen_time":screen_time
+    "Sleep_hours":sleep_hours
+    "Stu_name":stu_name 
+}
+print(dict1)
+df = pd.DataFrame(dict2)
+print[df]
